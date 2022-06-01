5389231704782b088bb8947cacc78cd37a6194e7
Jaydeep Vadalia <imjaydeepvadalia@gmail.com>
2022-06-01 09:19:03 +0530

    initial commit

---
+++ SwiftMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMVVM
+//
+//  Created by jaydeep vadalia on 01/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
